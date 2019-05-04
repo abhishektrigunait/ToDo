7c01ec929c09a0cb29c0273d0e1b2098f97eadd7
abhishektrigunait <abhishektrigunait0004@gmail.com>
2019-05-04 18:19:19 +0530

    first commit

---
+++ src/TodoItem.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function TodoItem(props) {
+    return (
+        <div calssName="todo-item">
+            <input 
+                type="checkbox" 
+                checked={props.item.completed} 
+                onChange={() => props.handleChange(props.item.id)}
+            />
+            {props.item.completed ? <i text-decoration="line-through">{props.item.text}</i> : <h4>{props.item.text}</h4>}
+            <hr />
+        </div>
+    )
+}
+
+export default TodoItem
---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Go for Jog",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "Hit the gym",
+        completed: false 
+    },
+    {
+        id: 3,
+        text: "Violin Class",
+        completed: false 
+    },
+    {
+        id: 4,
+        text: "Dance CLass",
+        completed: false
+    },
+    {
+        id: 5,
+        text: "Catch up on Development",
+        completed: false 
+    }
+]
+
+export default todosData
